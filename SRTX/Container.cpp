e6056e15791662ba65dd3db10604bdebd7980b94
Daniel Heater <daniel@heatertech.com>
2015-02-22 20:11:35 -0600

    Add a container class that allows an application to be loaded as a dynamically linked library and also replaced

---
+++ SRTX/Container.cpp
@@ -0,0 +1,81 @@
+#include "SRTX/Container.h"
+#include "base/XPRINTF.h"
+#include <dlfcn.h>
+
+
+namespace SRTX
+{
+
+    Container::Container(const char* const name, const char* const appname) :
+        Task(name),
+        m_appname(appname),
+        m_dll(NULL),
+        m_init(NULL),
+        m_execute(NULL),
+        m_terminate(NULL)
+    {
+    }
+
+
+    bool Container::init()
+    {
+        DPRINTF("Task %s, init\n", m_name);
+
+        m_dll = dlopen(m_appname, RTLD_LAZY);
+        if(NULL == m_dll)
+        {
+            EPRINTF("Error loading %s\n\t%s\n", m_appname, dlerror());
+            return false;
+        }
+
+        m_init = reinterpret_cast<bfunc_t>(dlsym(m_dll, "init"));
+        if(NULL == m_init)
+        {
+            EPRINTF("Error finding init() function\n");
+            return false;
+        }
+
+        m_execute = reinterpret_cast<bfunc_t>(dlsym(m_dll, "execute"));
+        if(NULL == m_execute)
+        {
+            EPRINTF("Error finding execute() function\n");
+            return false;
+        }
+
+        m_terminate = reinterpret_cast<vfunc_t>(dlsym(m_dll, "terminate"));
+        if(NULL == m_terminate)
+        {
+            EPRINTF("Error finding terminate() function\n");
+            return false;
+        }
+
+        return m_init();
+    }
+
+
+    bool Container::execute()
+    {
+        return m_execute();
+    }
+
+
+    void Container::terminate()
+    {
+        DPRINTF("Task %s, terminate\n", m_name);
+
+        if(m_terminate)
+        {
+            m_terminate();
+        }
+        m_init = NULL;
+        m_execute = NULL;
+        m_terminate = NULL;
+
+        if(m_dll)
+        {
+            dlclose(m_dll);
+        }
+        m_dll = NULL;
+    }
+
+} // namespace
---
+++ SRTX/Container.h
@@ -0,0 +1,75 @@
+#ifndef __SRTX_CONTAINER_H__
+#define __SRTX_CONTAINER_H__
+
+#include "SRTX/Task.h"
+
+
+namespace SRTX
+{
+
+    /**
+     * This class acts as a container for shared object libraries that
+     * implement the heart of the tasks functionality.
+     */
+    class Container: public SRTX::Task
+    {
+
+        public:
+
+            /**
+             * Constructor.
+             * @param name Task name.
+             * @param appname Pathname of the shared object library containing
+             * the application code to be run.
+             */
+            Container(const char* const name, const char* const appname);
+
+            /**
+             * Initialization routine.
+             * @return true on success or false on failure.
+             */
+            bool init();
+
+            /**
+             * This the the function that gets executed on a periodic basis
+             * each time this task is scheduler to run.
+             * @return Return true to continue execution or false to terminate
+             * the task.
+             */
+             bool execute();
+
+            /**
+             * Terminate routine.
+             */
+            void terminate();
+
+        private:
+
+            /**
+             * Shared object library pathname.
+             */
+            const char* const m_appname;
+
+            /**
+             * Pointer the the shared library application
+             */
+            void *m_dll;
+
+            /**
+             * Define function pointers.
+             */
+            typedef void (*vfunc_t)(void);
+            typedef bool (*bfunc_t)(void);
+            typedef int (*ifunc_t)(void);
+
+            /**
+             * Pointers to the application functions
+             */
+            bfunc_t m_init;
+            bfunc_t m_execute;
+            vfunc_t m_terminate;
+    };
+
+} // namespace
+
+#endif // __SRTX_CONTAINER_H__
