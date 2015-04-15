d7234d762d82562798da9e23e3910f416c7c3772
Daniel Heater <daniel@heatertech.com>
2015-04-14 21:34:00 -0500

    Add unit test for UDP clients.

---
+++ net/UDP_client_ut.cpp
@@ -0,0 +1,60 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <base/XPRINTF.h>
+
+#include "net/UDP_client.h"
+#include "net/UDP_client_ut.h"
+
+namespace net
+{
+    namespace
+    {
+        const int PORT = 7500;
+        const unsigned int BUFF_SIZE = 128;
+        pid_t echo_pid;
+    }
+
+    void UDP_client_ut::setUp()
+    {
+    }
+
+    void UDP_client_ut::tearDown()
+    {
+    }
+
+    void UDP_client_ut::spawn_echo()
+    {
+        echo_pid = fork();
+        if(0 == echo_pid)
+        {
+            execlp("./udpecho.py", "./udpecho.py", "-s", "7500", (char *)NULL);
+        }
+        else
+        {
+            sleep(1);
+        }
+    }
+
+    void UDP_client_ut::kill_echo()
+    {
+        kill(echo_pid, 9);
+    }
+
+    void UDP_client_ut::test_client()
+    {
+        UDP_client client("localhost", PORT);
+
+        /* Send a message and look for an echo return.
+         */
+        char sbuffer[] = "Hello world\n";
+        int nsend = strlen(sbuffer);
+        CPPUNIT_ASSERT_EQUAL(nsend, client.write(sbuffer, nsend));
+
+        char rbuffer[BUFF_SIZE];
+        int n = client.read(rbuffer, BUFF_SIZE);
+        rbuffer[n] = '\0';
+        CPPUNIT_ASSERT(0 == strcmp(rbuffer, sbuffer));
+    }
+} // namespace
---
+++ net/UDP_client_ut.h
@@ -0,0 +1,33 @@
+#ifndef __NET_UDP_CLIENT_UT_H__
+#define __NET_UDP_CLIENT_UT_H__
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace net
+{
+    class UDP_client_ut : public CppUnit::TestFixture
+    {
+
+        CPPUNIT_TEST_SUITE(UDP_client_ut);
+
+        CPPUNIT_TEST(spawn_echo);
+        CPPUNIT_TEST(test_client);
+        CPPUNIT_TEST(kill_echo);
+
+        CPPUNIT_TEST_SUITE_END();
+
+      public:
+        void setUp();
+        void tearDown();
+
+      protected:
+        void spawn_echo();
+        void kill_echo();
+        void test_client();
+    };
+
+    CPPUNIT_TEST_SUITE_REGISTRATION(UDP_client_ut);
+
+} // namespace
+
+#endif // __NET_UDP_CLIENT_UT_H__
