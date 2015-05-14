73fbb5ca3095a52a9c7fa6b34dd78bd8e7dc5024
Daniel Heater <daniel@heatertech.com>
2015-05-13 20:50:23 -0500

    Add requirements for direction cosine matrix

--- math/DCM_ut.h
+++ math/DCM_ut.h
@@ -10,10 +10,36 @@ namespace math
 
         CPPUNIT_TEST_SUITE(DCM_ut);
 
+        /**
+         * @verifies{math-7.3}
+         * @verifies{math-7.5}
+         * @verifies{math-7.6}
+         */
         CPPUNIT_TEST(test_DCM);
+
+        /**
+         * @verifies{math-7.5}
+         * @verifies{math-7.6}
+         */
         CPPUNIT_TEST(test_Euler_to_DCM);
+
+        /**
+         * @verifies{math-7.5}
+         * @verifies{math-7.9}
+         */
         CPPUNIT_TEST(test_multiply_vector);
+
+        /**
+         * @verifies{math-7.5}
+         * @verifies{math-7.10}
+         */
         CPPUNIT_TEST(test_transpose);
+
+        /**
+         * @verifies{math-7.5}
+         * @verifies{math-7.6}
+         * @verifies{math-7.11}
+         */
         CPPUNIT_TEST(test_inverse);
 
         CPPUNIT_TEST_SUITE_END();
