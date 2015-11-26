eafeab2c68e3a7e5cb9c80ba451760569c210acc
Daniel Heater <daniel@heatertech.com>
2015-11-26 10:25:55 -0600

    Add support for find_package(libSPRITE). Fixes #18

---
+++ cmake/libsprite-config.cmake
@@ -0,0 +1,10 @@
+# -config file for libSPRITE for use my find_package(libSPRITE)
+#
+# Defines the following macros
+# libSPRITE_INCLUDE_DIRS - Path to header files.
+# libSPRITE_LINK_DIRS - Path to libraries
+# libSPRITE_LIBRARIES - Libraries supplied by libSPRITE.
+
+set(libSPRITE_INCLUDE_DIRS "@CMAKE_INSTALL_PREFIX@/include/SPRITE")
+set(libSPRITE_LINK_DIRS "@CMAKE_INSTALL_PREFIX@/lib/SPRITE")
+set(libSPRITE_LIBRARIES SPRITE_SCALE SPRITE_SRTX SPRITE_util SPRITE_math SPRITE_net SPRITE_units)
